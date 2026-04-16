363f687b535ccba0a366d5c9aef23b49ed3f411a
SenkinAdmin <u1458349958@gmail.com>
2026-04-16 17:36:39 +0800

    Create bt_new.js

---
+++ bt_new.js
@@ -0,0 +1,107 @@
+if("undefined" != typeof bt && bt.hasOwnProperty("show_confirm")){
+    bt.show_confirm = function(title, msg, callback, error) {
+        layer.open({
+            type: 1,
+            title: title,
+            area: "365px",
+            closeBtn: 2,
+            shadeClose: true,
+            btn: [lan['public'].ok, lan['public'].cancel],
+            content: "<div class='bt-form webDelete pd20'>\
+                    <p style='font-size:13px;word-break: break-all;margin-bottom: 5px;'>" + msg + "</p>" + (error || '') + "\
+                </div>",
+            yes: function (index, layero) {
+                layer.close(index);
+                if (callback) callback();
+            }
+        });
+    }
+}
+if("undefined" != typeof bt && bt.hasOwnProperty("prompt_confirm")){
+    bt.prompt_confirm = function (title, msg, callback) {
+        layer.open({
+            type: 1,
+            title: title,
+            area: "350px",
+            closeBtn: 2,
+            btn: ['纭', '鍙栨秷'],
+            content: "<div class='bt-form promptDelete pd20'>\
+                <p>" + msg + "</p>\
+                </div>",
+            yes: function (layers, index) {
+                layer.close(layers)
+                if (callback) callback()
+            }
+        });
+    }
+}
+if("undefined" != typeof bt && bt.hasOwnProperty("compute_confirm")){
+    bt.compute_confirm = function (config, callback) {
+        layer.open({
+            type: 1,
+            title: config.title,
+            area: '430px',
+            closeBtn: 2,
+            shadeClose: true,
+            btn: [lan['public'].ok, lan['public'].cancel],
+            content:
+                '<div class="bt-form hint_confirm pd30">\
+          <div class="hint_title">\
+            <i class="hint-confirm-icon"></i>\
+            <div class="hint_con">' +
+                config.msg +
+                '</div>\
+          </div>\
+      </div>',
+            yes: function (layers, index) {
+                layer.close(layers)
+                if (callback) callback()
+            }
+        });
+    }
+}
+if("undefined" != typeof bt && bt.hasOwnProperty("input_confirm")){
+    bt.input_confirm = function (config, callback) {
+        layer.open({
+            type: 1,
+            title: config.title,
+            area: '430px',
+            closeBtn: 2,
+            shadeClose: true,
+            btn: [lan['public'].ok, lan['public'].cancel],
+            content:
+                '<div class="bt-form hint_confirm pd30">\
+                    <div class="hint_title">\
+                        <i class="hint-confirm-icon"></i>\
+                        <div class="hint_con">' +
+                config.msg +
+                '</div>\
+                    </div>\
+            </div>',
+            yes: function (layers, index) {
+                layer.close(layers);
+                if (callback) callback();
+            },
+        });
+    }
+}
+if(window.hasOwnProperty("SafeMessage")){
+    window.SafeMessage = function(j, h, g, f) {
+        if (f == undefined)  f = '';
+        var mess = layer.open({
+            type: 1,
+            title: j,
+            area: "350px",
+            closeBtn: 2,
+            shadeClose: true,
+            content: "<div class='bt-form webDelete pd20 pb70'><p>" + h + "</p>" + f + "<div class='bt-form-submit-btn'><button type='button' class='btn btn-danger btn-sm bt-cancel'>"+lan.public.cancel+"</button> <button type='button' id='toSubmit' class='btn btn-success btn-sm' >"+lan.public.ok+"</button></div></div>"
+        });
+        $(".bt-cancel").click(function(){
+            layer.close(mess);
+        });
+        $("#toSubmit").click(function() {
+            layer.close(mess);
+            g();
+        })
+    }
+}
